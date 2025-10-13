5e8ca75c444a46b4fe16e310db01b86ba7218d8b
matheusnb99 <matheusintelectual123@hotmail.com>
2025-10-13 21:58:07 +0200

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
 });
